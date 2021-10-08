042bd44794c1f353ae7efc4bdff54d915f647254
Shourya Solanki <shouryasolanki@MacBook-Pro.local>
2021-10-08 13:24:44 +0530

    CsvTest

--- src/test/java/com/anand/fileparser/CSVFileParserTest.java
+++ src/test/java/com/anand/fileparser/CSVFileParserTest.java
@@ -2,12 +2,43 @@ package com.anand.fileparser;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CSVFileParserTest {
 
-    //@Test
-   // public void CSVFileParserTest{
+    @Test
+    public void JSONParseFileTest() {
+        FileParser csvParser = null;
+        csvParser = new CSVFileParser("Csvtest.json");
 
+
+        List<Map<String, String>> actual = csvParser.parseFile();
+        //System.out.println(actual);
+
+        Map<String, String> data1 = actual.get(0);
+        assertEquals("Agali", data1.get("block_name"));
+        assertEquals("13.81073513", data1.get("block_lat"));
+        assertEquals("Agali", data1.get("block_name"));
+
+        Map<String, String> data2 = actual.get(1);
+        assertEquals("Hindupur", data2.get("block_name"));
+        assertEquals("5104", data2.get("block_id"));
+
+    }
+
+    @Test
+    public void itReturnsEmptyArray() {
+
+        FileParser csvParser = null;
+        csvParser = new JSONFileParser("test.json");
+
+        List<Map<String, String>> actual = csvParser.parseFile();
+
+        assertTrue(actual.size() == 0);
+
+    }
 
     }
--- src/test/java/com/anand/fileparser/JSONFileParserTest.java
+++ src/test/java/com/anand/fileparser/JSONFileParserTest.java
@@ -11,29 +11,30 @@ import static org.junit.jupiter.api.Assertions.*;
 public class JSONFileParserTest {
 
     @Test
-    public void JSONParseFileTest(){
-                    FileParser jsonParser = null;
-                    jsonParser = new JSONFileParser("Jsontest.json");
+    public void JSONParseFileTest() {
+        FileParser jsonParser = null;
+        jsonParser = new JSONFileParser("Jsontest.json");
 
 
-                List<Map<String, String>> actual =jsonParser.parseFile();
-                //System.out.println(actual);
+        List<Map<String, String>> actual = jsonParser.parseFile();
+        //System.out.println(actual);
 
-                Map<String,String> data1 = actual.get(0);
-                assertEquals("Agali", data1.get("block_name"));
+        Map<String, String> data1 = actual.get(0);
+        assertEquals("Agali", data1.get("block_name"));
 
-                Map<String,String> data2 = actual.get(1);
-                assertEquals("Penukonda", data2.get("block_name"));
-                assertEquals("5125", data2.get("block_id"));
+        Map<String, String> data2 = actual.get(1);
+        assertEquals("Penukonda", data2.get("block_name"));
+        assertEquals("5125", data2.get("block_id"));
 
     }
+
     @Test
     public void itReturnsEmptyArray() {
 
         FileParser jsonParser = null;
         jsonParser = new JSONFileParser("test.json");
 
-        List<Map<String, String>> actual =jsonParser.parseFile();
+        List<Map<String, String>> actual = jsonParser.parseFile();
 
         assertTrue(actual.size() == 0);
 
