fc53b1e8f08828246b50063c101471852c616563
Shourya Solanki <shouryasolanki@MacBook-Pro.local>
2021-10-08 11:33:02 +0530

    JsonTest

---
+++ src/test/java/com/anand/fileparser/CSVFileParserTest.java
@@ -0,0 +1,13 @@
+package com.anand.fileparser;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CSVFileParserTest {
+
+    //@Test
+   // public void CSVFileParserTest{
+
+
+    }
--- src/test/java/com/anand/fileparser/JSONFileParserTest.java
+++ src/test/java/com/anand/fileparser/JSONFileParserTest.java
@@ -3,9 +3,7 @@ package com.anand.fileparser;
 import org.junit.Test;
 import com.anand.fileparser.JSONFileParser;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,20 +11,31 @@ import static org.junit.jupiter.api.Assertions.*;
 public class JSONFileParserTest {
 
     @Test
-    public void parseFileTest(){
+    public void JSONParseFileTest(){
+                    FileParser jsonParser = null;
+                    jsonParser = new JSONFileParser("Jsontest.json");
 
-                FileParser jsonParser = new JSONFileParser("Jsontest.json");
 
                 List<Map<String, String>> actual =jsonParser.parseFile();
+                //System.out.println(actual);
 
-                actual.forEach(data -> {
-                    assertEquals("1577836800000", data.get("date"));//.forEach((k,v) -> );
+                Map<String,String> data1 = actual.get(0);
+                assertEquals("Agali", data1.get("block_name"));
 
+                Map<String,String> data2 = actual.get(1);
+                assertEquals("Penukonda", data2.get("block_name"));
+                assertEquals("5125", data2.get("block_id"));
 
-                });
+    }
+    @Test
+    public void itReturnsEmptyArray() {
 
+        FileParser jsonParser = null;
+        jsonParser = new JSONFileParser("test.json");
 
+        List<Map<String, String>> actual =jsonParser.parseFile();
 
+        assertTrue(actual.size() == 0);
 
     }
 }
